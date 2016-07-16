e6c50fe1514b4c5aee2f8e33835380c032c6d15e
corasan <henrypl360@gmail.com>
2016-07-16 15:13:51 -0400

    messages are being rendered woop

--- src/components/messagesList.js
+++ src/components/messagesList.js
@@ -5,18 +5,36 @@ import app from '../../firebaseInit';
 export default class MessagesList extends Component {
     constructor(props) {
         super(props);
-        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2 });
+        // const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2 });
+        // this.state = {
+        //     // dataSource: ds.cloneWithRows([{text: 'hi', sender: 'henry'}, {text: 'hello', sender: 'corasan'}])
+        //     dataSource: ds.cloneWithRows(props.messages)
+        // }
         this.state = {
-            // dataSource: ds.cloneWithRows([{text: 'hi', sender: 'henry'}, {text: 'hello', sender: 'corasan'}])
-            dataSource: ds.cloneWithRows(props.messages)
+            dataSource: new ListView.DataSource({
+                rowHasChanged: (row1, row2) => row1 !== row2,
+            })
         }
         console.log('2) Props:', this.props.messages);
     }
 
+    getDataSource(messages: Array<any>): ListView.DataSource {
+        if(!messages) return;
+        return this.state.dataSource.cloneWithRows(messages);
+    }
+
+    componentDidMount() {
+        this.setState({dataSource: this.getDataSource(this.props.messages)});
+    }
+
+    componentWillReceiveProps(props) {
+        this.setState({dataSource: this.getDataSource(props.messages)});
+    }
+
     renderRows = (data) => {
-        console.log('3) Data:', this.state.dataSource);
         return (
             <View style={{borderColor: 'black', borderWidth: 2, marginTop: 50}}>
+                <Text>{data.sender}</Text>
                 <Text>{data.text}</Text>
             </View>
         )
